406798bfceddecf320314c02cf0560b9fc3763a9
Confidence Selomo <selomomc@gmail.com>
2022-02-16 19:28:31 +0200

    Add project files.

---
+++ TillFloatManagement/Item.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+    public class Item
+    {
+
+    public string NamePrice { get; set; }
+   // public string Description { get; set; }
+    public string AmountPaid { get; set; }
+
+    }
+
